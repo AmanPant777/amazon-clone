cf4aea56ef049202551856d1f9231f9788e2f4fc
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-25 17:43:45 +0530

    add authentication with header signIn and SignOut

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,14 +1,19 @@
 export const initialState = {
   basket: [],
+  user: null,
 };
 export const getBasketTotal = (basket) => {
   return basket?.reduce((amount, item) => item.price + amount, 0);
 };
 export const reducer = (state, action) => {
-  console.log(action);
   switch (action.type) {
     case "ADD_TO_BASKET":
       return { ...state, basket: [...state.basket, action.item] };
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
     case "REMOVE_FROM_CART":
       const index = state.basket.findIndex(
         (basketitem) => basketitem.id === action.id
